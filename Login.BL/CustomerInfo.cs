191c1ae2552ac9f7da7559dba377eb2972ceab46
TechDeveloper375 <techdev375@gmail.com>
2023-03-16 14:00:55 -0400

    Created business layer for Customer class

---
+++ Login.BL/CustomerInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace Login.BL
+{
+    public class CustomerInfo
+    {
+        private string firstName;
+        private string lastName;
+        private long phone;
+        private string address;
+        private string city;
+        private string pasword;
+        private string email;
+    }
+        
+}
